4a77ef20c3c9fde161f13ece0885c322016856e6
Siddharth Kothari <sidkothari005@gmail.com>
2022-03-18 16:44:04 +0530

    added contact details

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env:{
+    mapbox_key:'pk.eyJ1Ijoic2lkZGhhcnRoa290aGFyaSIsImEiOiJjbDBncWFqdDAwMTVsM25vM2F0Zm8zdjFoIn0.9CD4loY-sJO_6HiFjXYlHg',
+  },
+};
--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Footer from '../components/Footer'
 import { useState } from 'react';
 import Header from '../components/Header'
+import { GlobeAltIcon, LocationMarkerIcon, MailIcon, PhoneIcon } from '@heroicons/react/solid'
+
+
+
 
 function Contact() {
   const [fname, setFname] = useState();
@@ -9,6 +13,15 @@ function Contact() {
   const [mail, setMail] = useState();
   const [number, setNumber] = useState();
   const [msg, setMsg] = useState();
+
+
+  const [viewport, setViewport] = useState({
+    width: '250px',
+    height: '250px',
+    latitude: 20.570516859764027,
+    longitude: 78.8757957791813,
+    zoom: 11,
+  });
 
   return (
     <div>
@@ -18,7 +31,7 @@ function Contact() {
           <link rel="icon" href="/logo-1.png" />
         </Head>
         <Header />
-        <main className='bg-gray-800 pb-10 grid grid-cols-1 lg:grid-cols-2'>
+        <main className='relative bg-gray-800 pb-10 grid grid-cols-1 lg:grid-cols-2'>
           <div>
             <div>
               <h1 className='text-white font-bold text-2xl text-center pt-5 pb-5 md:text-left md:ml-10 md:mr-10 md:text-3xl'>Get in touch</h1>
@@ -60,8 +73,36 @@ function Contact() {
                 <button type="submit" className="bg-black w-[10rem] rounded-md shadow-x p-2 ml-auto mr-auto  text-white active:scale-95 transition transform duration-100">Send Message
                 </button>
               </div>
-
             </form>
+
+          </div>
+
+          <div className=' ml-auto mr-auto mt-32'>
+            <img src="/map.png" alt="location" className='rounded-3xl hidden md:block mb-5' />
+
+            <h3 className='text-white text-4xl text-center font-bold'>Contact:</h3>
+
+            <div className='flex mt-7'>
+              <LocationMarkerIcon className='h-8 text-white flex' />
+              <p className='text-white flex pl-8 text-lg'>NH 44, MIDC, opp. Maruti Suzuki Seva, Hinganghat, Maharashtra 442301</p>
+            </div>
+
+            <div className='flex mt-4'>
+              <PhoneIcon className='h-8 text-white flex '/>
+              <p className='text-white flex pl-8 text-lg'>+91-8308307868</p>
+            </div>
+
+
+
+            <div className='flex mt-4'>
+              <GlobeAltIcon className='h-8 text-white flex '/>
+              <p className='text-white flex pl-8 text-lg'>www.lorryondemand.com</p>
+            </div>
+
+            <div className='flex mt-4'>
+              <MailIcon className='h-8 text-white flex '/>
+              <p className='text-white flex pl-8 text-lg'>contact@lorryondemand.com</p>
+            </div>
 
           </div>
         </main>
